6921039101d26960469fb0a58a660655e452fece
truong pham <batruong2262002@gmail.com>
2023-05-21 14:57:17 +0700

    finish

---
+++ src/main/java/com/example/DoAnJava/DoAnJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.DoAnJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoAnJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoAnJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DoAnJava/DoAnJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DoAnJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoAnJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

0e3d4a9d37e3c008d2a1530eeb7b3fd209b5f89d
khacdi26 <tkhacdi.26@gmail.com>
2023-05-30 08:06:05 +0700

    add repository

---
+++ src/main/java/com/example/DoAnJava/repository/ICategoryRepository.java
@@ -0,0 +1,9 @@
+package com.example.DoAnJava.repository;
+
+import com.example.DoAnJava.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ICategoryRepository extends JpaRepository<Category,Long> {
+}

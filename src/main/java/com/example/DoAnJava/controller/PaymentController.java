8671b293f40b171d7789690c8264cf9f408dc286
khacdi26 <tkhacdi.26@gmail.com>
2023-06-02 21:21:58 +0700

    add annotation

--- src/main/java/com/example/DoAnJava/controller/PaymentController.java
+++ src/main/java/com/example/DoAnJava/controller/PaymentController.java
@@ -6,13 +6,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import com.example.DoAnJava.entity.Orders;
+
 
 @Controller
 @RequestMapping("/pay")
 public class PaymentController {
     @GetMapping
-    public String listProduct()
-    {
+    public String addBookForm(Model model){
+
         return  "Payment/payment-form";
     }
 

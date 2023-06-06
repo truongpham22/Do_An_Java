17df8464e55028e3358ceb93a59197cca5eaeb48
Văn Nghĩa <nghiazorovn2304@gmail.com>
2023-06-06 21:12:53 +0700

    config call api

--- src/main/java/com/example/DoAnJava/controller/ProductController.java
+++ src/main/java/com/example/DoAnJava/controller/ProductController.java
@@ -1,6 +1,7 @@
 package com.example.DoAnJava.controller;
 
 import com.example.DoAnJava.DTO.CreateProductDto;
+import com.example.DoAnJava.DTO.ProductDto;
 import com.example.DoAnJava.entity.Category;
 import com.example.DoAnJava.entity.Product;
 import com.example.DoAnJava.services.FirebaseService;
@@ -12,53 +13,63 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-<<<<<<< HEAD
 import org.springframework.web.client.RestTemplate;
-=======
 import org.springframework.web.multipart.MultipartFile;
->>>>>>> e99ebda8e7c654606c7093026b21d9749c2f4845
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/api/product")
+@Controller
+@RequestMapping("/product")
 public class ProductController {
+    static final Logger logger = LoggerFactory.getLogger(ProductController.class);
     @Autowired
     private ProductService productService;
-
     @Autowired
     private FirebaseService firebaseService;
-    static final Logger logger = LoggerFactory.getLogger(ProductController.class);
+    @Autowired
+    private RestTemplate restTemplate;
 
     /*create api list products*/
+
+    @GetMapping("/view")
+    public String getView() {
+        String url = "http://localhost:8080/product/detail/1";
+        ProductDto product = this.restTemplate.getForObject(url, ProductDto.class);
+        System.out.println("A " + product.getId());
+        return "layout/layoutClient";
+    }
     @GetMapping("/list")
     @ResponseBody
-    public List<Product> getProductList(){
+    public List<Product> getProductList() {
         List<Product> product = this.productService.getAllProduct();
         return product;
     }
+
     @GetMapping("/detail/{id}")
-    public Product product(@PathVariable(value = "id") Long id){
+    @ResponseBody
+
+    public Product product(@PathVariable(value = "id") Long id) {
         Product product = productService.getProductById(id);
         return product;
     }
+
     // /product/category?category=abc
     @GetMapping("/category")
     @ResponseBody
-    public List<Product> getProductsByCate(@RequestParam("category") String category){
+    public List<Product> getProductsByCate(@RequestParam("category") String category) {
         List<Product> product = this.productService.getProductsByCategory(category);
         return product;
     }
 
     @GetMapping("/search")
     @ResponseBody
-    public List<Product> search(@RequestParam("search") String search){
-        return  this.productService.searchProducts(search);
+    public List<Product> search(@RequestParam("search") String search) {
+        return this.productService.searchProducts(search);
     }
 
     @PostMapping
     @ResponseBody
-    public Product create(@ModelAttribute() CreateProductDto product, @RequestParam("file") List<MultipartFile> file, @RequestParam("files") List<MultipartFile> files){
+    public Product create(@ModelAttribute() CreateProductDto product, @RequestParam("file") List<MultipartFile> file, @RequestParam("files") List<MultipartFile> files) {
         String url = this.firebaseService.uploadImages(file).get(0);
         System.out.println("url 0  " + url);
         List<String> url_list = this.firebaseService.uploadImages(files);
@@ -66,7 +77,6 @@ public class ProductController {
         String result = String.join(",", url_list);
         product.setImageList(result);
         System.out.println("url list  " + result);
-
         return this.productService.saveProduct(product);
     }
 
@@ -76,9 +86,6 @@ public class ProductController {
 //        productService.updateProduct(id, createProductDto);
 //        return "redirect:/products/";
 //    }
-
-
-
 
 
 }

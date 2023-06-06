b06a54780420201c82c9a371910c27a130967337
khacdi26 <tkhacdi.26@gmail.com>
2023-06-06 14:25:58 +0700

    call api

--- src/main/java/com/example/DoAnJava/controller/HomeController.java
+++ src/main/java/com/example/DoAnJava/controller/HomeController.java
@@ -1,8 +1,17 @@
 package com.example.DoAnJava.controller;
 
+import com.example.DoAnJava.entity.Product;
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/")
@@ -13,5 +22,20 @@ public class HomeController {
 
         return  "home/index";
     }
-
+    @GetMapping("/products")
+    public String products(Model model) {
+        String url = "http://localhost:8080/api/product/list";
+        RestTemplate restTemplate = new RestTemplate();
+        List result = restTemplate.getForObject(url, List.class);
+        model.addAttribute("products", result);
+        return "product/list";
+    }
+    @GetMapping("/detail/{id}")
+    public String detail(@PathVariable(value = "id") Long id , Model model) {
+        String url = "http://localhost:8080/api/product/detail/"+id;
+        RestTemplate restTemplate = new RestTemplate();
+        Product result = restTemplate.getForObject(url, Product.class);
+        model.addAttribute("product", result);
+        return "product/detail";
+    }
 }
--- src/main/java/com/example/DoAnJava/controller/ProductController.java
+++ src/main/java/com/example/DoAnJava/controller/ProductController.java
@@ -3,58 +3,36 @@ package com.example.DoAnJava.controller;
 import com.example.DoAnJava.entity.Category;
 import com.example.DoAnJava.entity.Product;
 import com.example.DoAnJava.services.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
-@Controller
-@RequestMapping("/product")
+@RestController
+@RequestMapping("/api/product")
 public class ProductController {
     @Autowired
     private ProductService productService;
     static final Logger logger = LoggerFactory.getLogger(ProductController.class);
 
+    /*create api list products*/
     @GetMapping("/list")
-    public String listProduct()
-    {
-        return  "product/list";
-    }
-
-    @GetMapping("/detail")
-    public String detailProduct(Model model){
-        Long id = 1l;
-        String name = "Sản phẩm test";
-        Double gia = 200.0;
-        String mota = "test mo ta san pham";
-
-        String hinh = "/img/4jpg.jpg";
-
-        Product product = new Product();
-        product.setId(id);
-        product.setName(name);
-        product.setPrice(gia);
-        product.setDescription(mota);
-        product.setUrlImageThumbnail(hinh);
-        model.addAttribute("product", product);
-        return "product/detail";
-    }
-
-    @GetMapping()
     @ResponseBody
     public List<Product> getProductList(){
         List<Product> product = this.productService.getAllProduct();
         return product;
     }
-
-
+    @GetMapping("/detail/{id}")
+    public Product product(@PathVariable(value = "id") Long id){
+        Product product = productService.getProductById(id);
+        return product;
+    }
     // /product/category?category=abc
     @GetMapping("/category")
     @ResponseBody

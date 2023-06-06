b0926b9b3b2f0d2185d7dfebf9c740a979da1d03
VuTrungNghia <vunghia467@gmail.com>
2023-06-06 18:34:05 +0700

    test2

--- src/main/java/com/example/DoAnJava/services/UserService.java
+++ src/main/java/com/example/DoAnJava/services/UserService.java
@@ -1,6 +1,7 @@
 package com.example.DoAnJava.services;
 
 import com.example.DoAnJava.entity.User;
+import com.example.DoAnJava.repository.IRoleRepository;
 import com.example.DoAnJava.repository.IUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,7 +14,8 @@ public class UserService {
     @Autowired
     private IUserRepository userRepository;
 
-
+    @Autowired
+    private IRoleRepository roleRepository;
     public List<User> getAllUser(){
         return userRepository.findAll();
     }
@@ -25,6 +27,15 @@ public class UserService {
 
     public void deleteUser(Long userId){
         userRepository.deleteById(userId);
+    }
+
+    public void save(User user){
+        userRepository.save(user);
+        Long userId = userRepository.getUserIdByUsername(user.getUsername());
+        Long roleId = roleRepository.getRoleIdByName("USER");
+        if(roleId !=0 && userId != 0){
+            userRepository.addRoleToUser(userId, roleId);
+        }
     }
 
 }

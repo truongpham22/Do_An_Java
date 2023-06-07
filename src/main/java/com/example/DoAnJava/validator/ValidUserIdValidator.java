1617537dab12b27623fdc4adf387fc6111bdeabf
minhnhut27 <nhutly19@gmail.com>
2023-06-07 08:25:57 +0700

    push

--- src/main/java/com/example/DoAnJava/validator/ValidUserIdValidator.java
+++ src/main/java/com/example/DoAnJava/validator/ValidUserIdValidator.java
@@ -4,3 +4,15 @@ import com.example.DoAnJava.validator.annotation.ValidUserId;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
+public class ValidUserIdValidator implements ConstraintValidator<ValidUserId, User> {
+
+    @Override
+    public boolean isValid(User user, ConstraintValidatorContext context){
+
+        if(user == null)
+
+            return true;
+
+        return user.getId() !=null;
+    }
+}

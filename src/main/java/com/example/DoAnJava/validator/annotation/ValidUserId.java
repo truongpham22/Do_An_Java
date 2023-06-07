6f9727236be352c3c44bddc4f3228dd144ba83f7
minhnhut27 <nhutly19@gmail.com>
2023-06-07 08:06:18 +0700

    push

--- src/main/java/com/example/DoAnJava/validator/annotation/ValidUserId.java
+++ src/main/java/com/example/DoAnJava/validator/annotation/ValidUserId.java
@@ -16,4 +16,10 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 @Constraint(validatedBy = ValidUsernameValidator.class)
 @Documented
 public @interface ValidUserId {
+    String message() default "Invalid User ID";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
 }

cc91916b2f29df1e44905e3fbdd9b9221c96c956
minhnhut27 <nhutly19@gmail.com>
2023-06-07 08:01:58 +0700

    push

--- src/main/java/com/example/DoAnJava/validator/ValidUsernameValidator.java
+++ src/main/java/com/example/DoAnJava/validator/ValidUsernameValidator.java
@@ -4,14 +4,4 @@ import com.example.DoAnJava.validator.annotation.ValidUsername;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import org.springframework.beans.factory.annotation.Autowired;
-public class ValidUsernameValidator implements ConstraintValidator<ValidUsername, String> {
 
-    @Autowired
-    private IUserRepository userRepository;
-    @Override
-    public boolean isValid(String username, ConstraintValidatorContext context){
-        if(userRepository == null)
-            return true;
-        return userRepository.findByUsername(username) == null;
-    }
-}

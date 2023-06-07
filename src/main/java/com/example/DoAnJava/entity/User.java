8eccf5f864851dc8c2d56c7d66e2016bdef03889
minhnhut27 <nhutly19@gmail.com>
2023-06-07 07:57:13 +0700

    push

--- src/main/java/com/example/DoAnJava/entity/User.java
+++ src/main/java/com/example/DoAnJava/entity/User.java
@@ -15,23 +15,43 @@ import java.util.Set;
 @Data
 @Entity
 @Table(name = "user")
-<<<<<<< HEAD
-public class User {
-=======
 public class User  {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(name = "username", length = 50, nullable = false, unique = true)
     @NotBlank(message = "Username is required")
->>>>>>> b1803ca88404c6cc8fc8ec92ce2b7342355e954c
-
-
     @ManyToMany
     @JoinTable(name = "user_role",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name ="role_id"))
     private Set<Role> roles = new HashSet<>();
+    @Size(max = 50, message = "Username must be less than 50 characters")
+    @ValidUsername
+    private String username;
+
+    @Column(name = "password", length = 250, nullable = false)
+    @NotBlank(message = "Password is required")
+    private String password;
+
+    @Column(name = "email", length = 50)
+    @Size(max = 50, message = "Email must be less than 50 characters")
+    private String email;
+
+    @Column(name = "name", length = 50, nullable = false)
+    @Size(max = 50, message = "Your name must be less than 50 characters")
+    @NotBlank(message = "Your name is required")
+    private String name;
+
+    @OneToMany(mappedBy = "user" ,cascade = CascadeType.ALL)
+    private Set<Orders> orders;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id",referencedColumnName = "id")
+    private Role role;
+
+    @Column(name = "phoneNumber")
+    private String phoneNumber;
 
 
 

3a068297d808e390e9ae8413e93a5142f9d0f38e
minhnhut27 <nhutly19@gmail.com>
2023-06-07 08:09:36 +0700

    push

--- src/main/java/com/example/DoAnJava/controller/UserController.java
+++ src/main/java/com/example/DoAnJava/controller/UserController.java
@@ -18,35 +18,4 @@ import java.util.List;
 @Controller
 public class UserController {
 
-    @Autowired
-    private UserService userService;
-    @GetMapping("/login")
-    public String login() {
-        return "user/login";
-    }
-    @GetMapping("/register")
-    public String register(Model model) {
-        model.addAttribute("user", new User());
-
-        return "user/register";
-    }
-    @PostMapping("/register")
-
-    public String register(@Valid @ModelAttribute("user") User user,
-                           BindingResult bindingResult, Model model) {
-        if (bindingResult.hasErrors()) {
-            List<FieldError> errors = bindingResult.getFieldErrors();
-            for (FieldError error : errors) {
-                model.addAttribute(error.getField() + "_error",
-                        error.getDefaultMessage());
-            }
-            return "user/register";
-        }
-        user.setPassword(new
-                BCryptPasswordEncoder().encode(user.getPassword()));
-        userService.saveUser(user);
-
-        return "redirect:/login";
-
-    }
 }

f9bb2a2bb3204675f7d9deac7690a3552eee1b80
minhnhut27 <nhutly19@gmail.com>
2023-06-07 08:04:18 +0700

    push

--- src/main/java/com/example/DoAnJava/repository/IUserRepository.java
+++ src/main/java/com/example/DoAnJava/repository/IUserRepository.java
@@ -9,6 +9,18 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface IUserRepository extends JpaRepository<User, Long> {
+    @Query("SELECT u FROM User u WHERE u.username = ?1")
 
+    User findByUsername(String username);
 
+    @Modifying
+    @Transactional
+    @Query(value = "INSERT INTO user_role (user_id, role_id)" + "VALUES (?1, ?2", nativeQuery = true)
+    void addRoleToUser(Long userId, Long roleId);
+
+    @Query("SELECT u.id FROM User u WHERE u.username = ?1")
+    Long getUserIdByUsername(String username);
+
+    @Query(value = "SELECT r.name FROM role r INNER JOIN user_role ur" + "ON r.id = ur.role_id WHERE ur.user_id = ?1", nativeQuery = true)
+    String[] getRolesOfUser(Long userId);
 }

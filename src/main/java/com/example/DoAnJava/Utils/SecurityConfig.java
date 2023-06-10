59fba88704ab40792e1d69793d82b6acc07052aa
VuTrungNghia <vunghia467@gmail.com>
2023-06-10 18:34:03 +0700

    push lai

--- src/main/java/com/example/DoAnJava/Utils/SecurityConfig.java
+++ src/main/java/com/example/DoAnJava/Utils/SecurityConfig.java
@@ -42,7 +42,7 @@ public class SecurityConfig {
         return http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers( "/**", "/js/**", "/", "/register","/errors")
+                        .requestMatchers( "/**", "/js/**", "/", "/register","/error")
                         .permitAll()
                         .requestMatchers("/admin")
                         .hasAnyAuthority("ADMIN")
--- src/main/java/com/example/DoAnJava/repository/IUserRepository.java
+++ src/main/java/com/example/DoAnJava/repository/IUserRepository.java
@@ -14,12 +14,12 @@ public interface IUserRepository extends JpaRepository<User, Long> {
 
     @Modifying
     @Transactional
-    @Query(value = "INSERT INTO user_role (user_id, role_id)" + "VALUES (?1, ?2", nativeQuery = true)
+    @Query(value = "INSERT INTO user_role (user_id, role_id)" + " VALUES (?1, ?2", nativeQuery = true)
     void addRoleToUser(Long userId, Long roleId);
 
     @Query("SELECT u.id FROM User u WHERE u.username = ?1")
     Long getUserIdByUsername(String username);
 
-    @Query(value = "SELECT r.name FROM role r INNER JOIN user_role ur " + "ON r.id = ur.role_id WHERE ur.user_id = ?1", nativeQuery = true)
+    @Query(value = "SELECT r.name FROM role r INNER JOIN user_role ur " + " ON r.id = ur.role_id WHERE ur.user_id = ?1", nativeQuery = true)
     String[] getRolesOfUser(Long userId);
 }

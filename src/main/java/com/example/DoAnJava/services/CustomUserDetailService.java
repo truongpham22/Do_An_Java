b4dc0d5d51a1c535160f836eacafb88953a83b5d
VuTrungNghia <vunghia467@gmail.com>
2023-06-10 08:58:18 +0700

    phan quyen

--- src/main/java/com/example/DoAnJava/services/CustomUserDetailService.java
+++ src/main/java/com/example/DoAnJava/services/CustomUserDetailService.java
@@ -17,14 +17,11 @@ public class CustomUserDetailService implements UserDetailsService {
 
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
-
+    public UserDetails loadUserByUsername(String username ) throws UsernameNotFoundException
+    {
         User user = userRepository.findByUsername(username);
-
-        if(user == null)
-
-            throw new UsernameNotFoundException("User not found");
-
-        return new CustomUserDetail(user, userRepository);
+        if (user == null)
+            throw  new UsernameNotFoundException("User not found");
+        return new CustomUserDetail(user,userRepository);
     }
 }

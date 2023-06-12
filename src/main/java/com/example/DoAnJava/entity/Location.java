d57e9efd7e026fdb18eea37be31748ae2d36708b
khacdi26 <tkhacdi.26@gmail.com>
2023-06-12 18:42:02 +0700

    update database

---
+++ src/main/java/com/example/DoAnJava/entity/Location.java
@@ -0,0 +1,30 @@
+package com.example.DoAnJava.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+//@JsonIgnoreProperties({"products"})
+@Table(name = "location")
+public class Location {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+    @OneToMany(mappedBy = "location" ,cascade = CascadeType.ALL)
+    @JsonIgnore
+    @JsonBackReference("location")
+    private Set<User> users;
+}
